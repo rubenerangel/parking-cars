5ce7c1d2d8596d67410d056a359bcc4c5906a60c
rubenerangel <rubenrang@gmail.com>
2020-10-22 12:12:11 -0400

    Create Model and table reduction

---
+++ database/migrations/2020_10_22_155822_create_reductions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReductionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reductions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->decimal('percentage', 8,2)->default(0);
+            $table->time('time', 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reductions');
+    }
+}
